73291c006c9d2e460b5e3b9546d8720894a754b9
kanakWeb <kanakwebdev@gmail.com>
2025-06-21 23:38:06 +0600

    Add project files.

---
+++ StudentCourses/Controllers/StudentCoursesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class StudentCoursesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public StudentCoursesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    private CourseStatus GetStatus(int marks)
+    {
+        if (marks < 50) return CourseStatus.NeedsImprovement;
+        if (marks < 80) return CourseStatus.Good;
+        return CourseStatus.Excellent;
+    }
+
+    public async Task<IActionResult> Index(CourseStatus? status)
+    {
+        var query = _context.StudentCourses.AsQueryable();
+
+        if (status != null)
+        {
+            query = query.Where(s => s.Status == status);
+        }
+
+        ViewBag.StatusList = Enum.GetValues(typeof(CourseStatus)).Cast<CourseStatus>().ToList();
+        ViewBag.SelectedStatus = status;
+
+        return View(await query.ToListAsync());
+    }
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("StudentName,CourseTitle,TotalMarks")] StudentCourse studentCourse)
+    {
+        if (ModelState.IsValid)
+        {
+            studentCourse.Status = GetStatus(studentCourse.TotalMarks);
+            _context.Add(studentCourse);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        return View(studentCourse);
+    }
+}
---
+++ StudentCourses/Models/StudentCourse.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+public class StudentCourse
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string? StudentName { get; set; }
+
+    [Required]
+    public string? CourseTitle { get; set; }
+
+    [Range(0, 100)]
+    public int TotalMarks { get; set; }
+
+    public CourseStatus Status { get; set; }
+}
+public enum CourseStatus
+{
+    NeedsImprovement,
+    Good,
+    Excellent
+}
+
